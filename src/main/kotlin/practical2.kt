001c56aaca0ed6d1e0fe6af704f4f9e502d1ddd8
Siddhant <siddhantpatel209@gmail.com>
2022-08-22 21:17:04 +0530

    prac

--- src/main/kotlin/practical2.kt
+++ src/main/kotlin/practical2.kt
@@ -1,17 +1,13 @@
 fun main() {
     var a: Int = 10
-//    println("$a")
-//    Integer To Double
     var b: Double = a.toDouble()
     println("$b")
 
-//    String To Integer
-    var c: String = "54545"
+    var c: String = "545"
     var d: Int = c.toInt()
     println("$d")
 
-//    String To Double
-    var e: String = "4754"
+    var e: String = "44"
     var f: Double = e.toDouble()
     println("$f")
 }
